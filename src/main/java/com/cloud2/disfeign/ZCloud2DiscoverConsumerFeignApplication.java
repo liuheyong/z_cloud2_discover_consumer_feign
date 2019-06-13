ac502d65a0efedb016b452db92683e89dd8a0596
liuheyong <17682347237@163.com>
2019-06-13 11:47:35 +0800

    commit

---
+++ src/main/java/com/cloud2/disfeign/ZCloud2DiscoverConsumerFeignApplication.java
@@ -0,0 +1,13 @@
+package com.cloud2.disfeign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZCloud2DiscoverConsumerFeignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZCloud2DiscoverConsumerFeignApplication.class, args);
+    }
+
+}
